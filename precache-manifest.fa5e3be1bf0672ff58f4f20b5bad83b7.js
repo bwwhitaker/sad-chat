cf3f1664983b68caf08062cb95ce3cd17939b1af
Brian Whitaker <bwwhitaker@gmail.com>
2020-07-11 17:13:52 -0700

    Updates

---
+++ precache-manifest.fa5e3be1bf0672ff58f4f20b5bad83b7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b149b9d62d4b3655b8c2c4d9d3a17bf",
+    "url": "/sad-chat/index.html"
+  },
+  {
+    "revision": "a2ffda242affa5a6d43e",
+    "url": "/sad-chat/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d9f832a5a67b8402f75d",
+    "url": "/sad-chat/static/css/main.a2dc6b42.chunk.css"
+  },
+  {
+    "revision": "a2ffda242affa5a6d43e",
+    "url": "/sad-chat/static/js/2.cd0c74bd.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sad-chat/static/js/2.cd0c74bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9f832a5a67b8402f75d",
+    "url": "/sad-chat/static/js/main.241a7396.chunk.js"
+  },
+  {
+    "revision": "dad9888995b9bea4f343",
+    "url": "/sad-chat/static/js/runtime-main.29e1b9a0.js"
+  }
+]);
